39ae50f1f56146189c5b1a7975ccdf3e1fbcfd84
王东煜 <213633@nbcb.cn>
2022-03-21 19:13:24 +0800

    提交Framework和Bundle

---
+++ FrameworkFramework/a.framework/Headers/helloobject.h
@@ -0,0 +1,16 @@
+//
+//  helloobject.h
+//  a
+//
+//  Created by nbcbdev-1 on 2022/3/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface helloobject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
